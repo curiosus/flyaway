c75d0c2ab811ec9b68b4567ab9ff6f95c8e8c298
curiosus <curiosus@gmail.com>
2024-11-23 16:03:46 -0700

    add movement

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,32 @@ use macroquad::prelude::*;
 
 #[macroquad::main("Fly Away")]
 async fn main() {
+
+    let mut x = screen_width() / 2.0;
+    let mut y = screen_height() / 2.0;
+    let speed = 6.0;
+
     loop {
         clear_background(DARKPURPLE);
+
+        if is_key_down(KeyCode::Right) || is_key_down(KeyCode::D) {
+            x += speed;
+        }
+
+        if is_key_down(KeyCode::Left) || is_key_down(KeyCode::A) {
+            x -= speed;
+        }
+
+        if is_key_down(KeyCode::Down) || is_key_down(KeyCode::S) {
+            y += speed;
+        }
+
+        if is_key_down(KeyCode::Up) || is_key_down(KeyCode::W) {
+            y -= speed;
+        }
+
+        draw_circle(x, y, 16.0, YELLOW);
+
         next_frame().await
     }
 }
