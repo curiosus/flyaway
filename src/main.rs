8480bbd96980df4b039981515ec0701df1921da9
curiosus <curiosus@gmail.com>
2024-11-23 19:24:14 -0700

    collisions and game over

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,28 @@ struct Shape {
     speed: f32,
     x: f32,
     y: f32,
+
 }
-    
-const MOVEMENT_SPEED: f32  = 200.0;
+
+impl Shape {
+    fn collides_with(&self, other: &Self) -> bool {
+        self.rect().overlaps(&other.rect())
+    }
+
+    fn rect(&self) -> Rect {
+        Rect {
+            x: self.x - self.size / 2.0,
+            y: self.y - self.size / 2.0,
+            w: self.size,
+            h: self.size,
+        }
+    }
+}
+
+const MOVEMENT_SPEED: f32 = 200.0;
 
 #[macroquad::main("Fly Away")]
 async fn main() {
-
     rand::srand(miniquad::date::now() as u64);
 
     let mut squares = vec![];
@@ -23,56 +38,84 @@ async fn main() {
         y: screen_height() / 2.0,
     };
 
+    let mut gameover = false;
+
     loop {
+        if !gameover {
+            clear_background(DARKPURPLE);
 
-        clear_background(DARKPURPLE);
+            let delta_time = get_frame_time();
 
-        let delta_time = get_frame_time();
+            if is_key_down(KeyCode::Right) || is_key_down(KeyCode::D) {
+                circle.x += MOVEMENT_SPEED * delta_time;
+            }
 
-        if is_key_down(KeyCode::Right) || is_key_down(KeyCode::D) {
-            circle.x += MOVEMENT_SPEED * delta_time;
+            if is_key_down(KeyCode::Left) || is_key_down(KeyCode::A) {
+                circle.x -= MOVEMENT_SPEED * delta_time;
+            }
+
+            if is_key_down(KeyCode::Down) || is_key_down(KeyCode::S) {
+                circle.y += MOVEMENT_SPEED * delta_time;
+            }
+
+            if is_key_down(KeyCode::Up) || is_key_down(KeyCode::W) {
+                circle.y -= MOVEMENT_SPEED * delta_time;
+            }
+
+            circle.x = clamp(circle.x, 0.0, screen_width());
+            circle.y = clamp(circle.y, 0.0, screen_height());
+
+            if rand::gen_range(0, 99) >= 95 {
+                let size = rand::gen_range(16.0, 64.0);
+                squares.push(Shape {
+                    size,
+                    speed: rand::gen_range(50.0, 150.0),
+                    x: rand::gen_range(size / 2.0, screen_width() - size / 2.0),
+                    y: -size,
+                });
+            }
+
+            for square in &mut squares {
+                square.y += square.speed * delta_time;
+            }
+
+            squares.retain(|square| square.y < screen_height() + square.size);
+
+            if squares.iter().any(|square| circle.collides_with(square)) {
+                gameover = true;
+            }
+
+
+            draw_circle(circle.x, circle.y, circle.size / 2.0, YELLOW);
+
+            for square in &squares {
+                draw_rectangle(
+                    square.x - square.size / 2.0,
+                    square.y - square.size / 2.0,
+                    square.size,
+                    square.size,
+                    GREEN,
+                );
+            }
         }
 
-        if is_key_down(KeyCode::Left) || is_key_down(KeyCode::A) {
-            circle.x -= MOVEMENT_SPEED * delta_time;
+        if gameover && is_key_pressed(KeyCode::Space) {
+            println!("pressing space");
+            squares.clear();
+            circle.x = screen_width() / 2.0;
+            circle.y = screen_height() / 2.0;
+            gameover = false;
         }
 
-        if is_key_down(KeyCode::Down) || is_key_down(KeyCode::S) {
-            circle.y += MOVEMENT_SPEED * delta_time;
-        }
-
-        if is_key_down(KeyCode::Up) || is_key_down(KeyCode::W) {
-            circle.y -= MOVEMENT_SPEED * delta_time;
-        }
-
-        circle.x = clamp(circle.x, 0.0, screen_width());
-        circle.y = clamp(circle.y, 0.0, screen_height());
-
-        if rand::gen_range(0, 99) >= 95 {
-            let size = rand::gen_range(16.0, 64.0);
-            squares.push(Shape {
-                size,
-                speed: rand::gen_range(50.0, 150.0),
-                x: rand::gen_range(size / 2.0, screen_width() - size / 2.0),
-                y: -size,
-            });
-        }
-
-        for square in &mut squares {
-            square.y += square.speed * delta_time;
-        }
-
-        squares.retain(|square| square.y < screen_height() + square.size);
-
-        draw_circle(circle.x, circle.y, circle.size / 2.0, YELLOW);
-
-        for square in &squares {
-            draw_rectangle ( 
-                square.x - square.size / 2.0,
-                square.y - square.size / 2.0,
-                square.size,
-                square.size,
-                GREEN,
+        if gameover {
+            let text = "GAME OVER!";
+            let text_dimensions = measure_text(text, None, 50, 1.0);
+            draw_text(
+                text,
+                screen_width() / 2.0 - text_dimensions.width / 2.0,
+                screen_height() / 2.0,
+                50.0,
+                RED,
             );
         }
 
